81db4138a4e439e7de0c72d6e56528f5ba39d7a6
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-22 16:53:27 +0800

    🎨 update typescript sample  file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const num1: number = 5;
-const num2: number = 4;
+const num1 = 5;
+const num2 = 4;
 
 function sum(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-console.log(num1, num2);
+console.log(sum(num1, num2));
