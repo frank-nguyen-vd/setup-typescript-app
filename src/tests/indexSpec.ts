7424492d6bec7f8788271209f059670918d161fd
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-27 21:34:12 +0800

    Create a test suite

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,7 @@
 import { myFunc } from '../index';
 
-it('expect myFunc(5) to equal 25', () => {
-    expect(myFunc(5)).toEqual(25);
+describe('Sample function', () => {
+    it('should multiply 5 by 5 and returns 25', () => {
+        expect(myFunc(5)).toEqual(25);
+    });
 });
