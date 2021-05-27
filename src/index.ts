ce6c45a3186bc8e50d475b68379519c1021419dd
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-27 22:48:34 +0800

    Write sample endpoint tests with supertest

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import express from 'express';
+
+export const app = express();
+const port = 5000;
+
+app.get('/api', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
 export const myFunc = (num: number) => {
     return num * num;
 };
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,4 +1,15 @@
-import { myFunc } from '../index';
+import supertest from 'supertest';
+import { app, myFunc } from '../index';
+
+const request = supertest(app);
+
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async (done) => {
+        const response = await request.get('/api');
+        expect(response.status).toBe(200);
+        done();
+    });
+});
 
 describe('Sample function', () => {
     it('should multiply 5 by 5 and returns 25', () => {
