516fbec69a331ad6f84197995a2453cefa33df87
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-29 10:38:40 +0800

    :sparkles: read/write a txt file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import express from 'express';
+import { promises as fs } from 'fs';
 
 export const app = express();
 const port = 5000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/write', async (req, res) => {
+    const myFile = await fs.open('data/myFile.txt', 'a+');
+    await myFile.write('Hello World\n');
+    res.send('File content is written');
+});
+
+app.get('/read', async (req, res) => {
+    const myFile = await fs.readFile('data/myFile.txt', 'utf-8');
+    res.send(myFile);
+});
+
+app.get('/read-abit', async (req, res) => {
+    const buff = Buffer.alloc(30);
+    const myFile = await fs.open('data/myFile.txt', 'r');
+    await myFile.read(buff, 0, 5);
+
+    const decode: string[] = [];
+    buff.forEach((code) => {
+        decode.push(String.fromCharCode(code));
+    });
+
+    res.send(decode.join(''));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
